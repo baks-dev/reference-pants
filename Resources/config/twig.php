0257a43e946745553df48b321abe8457451a4445
kingstar <admin@baks.dev>
2024-06-08 21:13:52 +0300

    Baks Development

--- Resources/config/twig.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Symfony\Component\DependencyInjection\Loader\Configurator;
-
-use Symfony\Config\TwigConfig;
-
-return static function(TwigConfig $twig) {
-	$twig->path(__DIR__.'/../view', 'reference-pants');
-};
