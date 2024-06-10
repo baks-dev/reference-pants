946d09e38837174a063ad08265886a2fe2e550fa
kingstar <admin@baks.dev>
2024-06-10 10:38:44 +0300

    Baks Development

--- BaksDevReferencePantsBundle.php
+++ BaksDevReferencePantsBundle.php
@@ -22,4 +22,39 @@ class BaksDevReferencePantsBundle extends AbstractBundle
     public const NAMESPACE = __NAMESPACE__.'\\';
 
     public const PATH = __DIR__.DIRECTORY_SEPARATOR;
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $services = $container->services()
+            ->defaults()
+            ->autowire()
+            ->autoconfigure();
+
+        //        $services->load(self::NAMESPACE, self::PATH)
+        //            ->exclude([
+        //                self::PATH.'{Entity,Resources,Type}',
+        //                self::PATH.'**/*Message.php',
+        //                self::PATH.'**/*DTO.php',
+        //            ]);
+
+        $services->load(
+            self::NAMESPACE.'Form\\',
+            self::PATH.'Form'
+        );
+
+        $services->load(
+            self::NAMESPACE.'Type\Sizes\\',
+            self::PATH.'Type/Sizes'
+        );
+
+        $services->load(
+            self::NAMESPACE.'Listeners\\',
+            self::PATH.'Listeners'
+        );
+
+        $services->load(
+            self::NAMESPACE.'Twig\\',
+            self::PATH.'Twig'
+        );
+    }
 }
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # BaksDev Pants Sizes
 
-[![Version](https://img.shields.io/badge/version-7.1.1-blue)](https://github.com/baks-dev/reference-pants/releases)
+[![Version](https://img.shields.io/badge/version-7.1.2-blue)](https://github.com/baks-dev/reference-pants/releases)
 ![php 8.3+](https://img.shields.io/badge/php-min%208.3-red.svg)
 
 Библиотека размеров брюк (25/30 ... 38/40)
